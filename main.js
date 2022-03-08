87dbe074f9fe6c137fd825203268fe7a2fe4d567
arjun3637 <77474063+arjun3637@users.noreply.github.com>
2022-03-08 18:02:24 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,140 @@
+var GameState = {
+    preload: function(){
+        this.load.image('background', 'images/background.png');
+        this.load.image('arrow', 'images/arrow.png');
+
+        this.load.spritesheet('chicken', 'images/chicken_spritesheet.png', 131, 200, 3);
+        this.load.spritesheet('horse', 'images/horse_spritesheet.png', 212, 200, 3);
+        this.load.spritesheet('pig', 'images/pig_spritesheet.png', 297, 200, 3);
+        this.load.spritesheet('sheep', 'images/sheep_spritesheet.png', 244, 200, 3);
+
+        // this.load.audio('chickenSound', ['audio/chicken.ogg','audio/chicken.mp3']); 
+        // this.load.audio('horseSound', ['audio/horse.ogg','audio/horse.mp3']);
+        // this.load.audio('pigSound', ['audio/pig.ogg','audio/pig.mp3']);
+        // this.load.audio('sheepSound', ['audio/sheep.ogg','audio/sheep.mp3']);
+
+
+    },
+    create: function(){
+        //screen scale
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        //screen center allignment
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+
+        this.background = this.game.add.sprite(0, 0, 'background');
+
+        //group for animals
+        var animalData = [
+            {key: 'chicken', text: 'CHICKEN'},
+            {key: 'horse', text: 'HORSE'},
+            {key: 'pig', text: 'PIG'},
+            {key: 'sheep', text: 'SHEEP'}
+        ];
+
+
+        this.animals = this.game.add.group();
+
+        var self = this;
+        var animal;
+        animalData.forEach(function(element){
+            animal = self.animals.create(-1000, this.game.world.centerY, element.key, 0);
+
+            animal.customParams = {text: element.key};
+            animal.anchor.setTo(0.5);
+
+            animal.animations.add('animate',[0, 1, 2, 1, 0, 1], 3, false);
+
+            animal.inputEnabled = true;
+            animal.input.pixelPerfectClick = true;
+            animal.events.onInputDown.add(self.animateAnimal, self);
+        });
+
+       //chickenSound = this.sound.add("chickenSound", { loop: false });
+
+        this.currentAnimal = this.animals.next();
+        this.currentAnimal.position.set(this.game.world.centerX, this.game.world.centerY)
+        this.showText(this.currentAnimal);
+
+        //Left arrow
+        this.leftArrow = this.game.add.sprite(60, this.game.world.centerY, 'arrow');
+        this.leftArrow.anchor.setTo(0.5);
+        this.leftArrow.scale.x = -1
+        this.leftArrow.customParams = {direction: -1};
+        //user input for left arrow
+        this.leftArrow.inputEnabled = true;
+        this.leftArrow.input.pixelPerfectClick = true;
+        this.leftArrow.events.onInputDown.add(this.switchAnimal, this);
+
+        //Right arrow
+        this.rightArrow = this.game.add.sprite(580, this.game.world.centerY, 'arrow');
+        this.rightArrow.anchor.setTo(0.5);
+        this.rightArrow.customParams = {direction: 1};
+        //user input for right arrow
+        this.rightArrow.inputEnabled = true;
+        this.rightArrow.input.pixelPerfectClick = true;
+        this.rightArrow.events.onInputDown.add(this.switchAnimal, this);
+    },
+    update: function(){   
+    },
+    animateAnimal: function(sprite, event) {
+        sprite.play('animate'); //  chickenSound.play();
+    },
+    switchAnimal: function(sprite, event) {
+        
+        if(this.isMoving){
+            return false;
+        }
+        this.isMoving = true;
+        this.animalText.visible = false;
+        var newAnimal, endX;
+
+        if (sprite. customParams.direction > 0) {
+            newAnimal = this.animals.next();
+            newAnimal.x = -newAnimal.width/2; 
+            endX = 640 + this.currentAnimal.width/2;
+            }else {
+            newAnimal = this.animals.previous() ;
+            newAnimal.x = 640 + newAnimal.width/2;
+            endX = -this.currentAnimal.width/2;
+            }
+
+            var newAnimalMovement = this.game.add.tween(newAnimal);
+            newAnimalMovement.to({x: this.game.world.centerX}, 1000);
+            newAnimalMovement.onComplete.add(function(){
+                this.isMoving = false
+                this.showText(this.currentAnimal);
+            }, this);
+            newAnimalMovement.start();
+
+            var currentAnimalMovement = this.game.add.tween(this.currentAnimal);
+            currentAnimalMovement.to({x: endX}, 1000);
+            currentAnimalMovement.start();
+
+
+            this.currentAnimal = newAnimal;
+    },
+
+
+
+
+
+    showText: function(animal){
+        if(!this.animalText){
+            this.animalText = this.game.add.text(this.game.width/2, this.game.height * 0.85, '');
+
+            this.animalText.anchor.setTo(0.5);
+        }
+        this.animal.setText(animal.customParams.text);
+        this.animalText.visible = true;
+    },
+
+    
+};
+
+var game = new Phaser.Game(640, 360, Phaser.AUTO);
+
+game.state.add('GameState', GameState);
+game.state.start('GameState');
+
+
